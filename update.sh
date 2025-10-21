118c21e495180143de7909cdba6ec6c3db29f5ed
Ubuntu <ubuntu@vps-dde8362e.vps.ovh.net>
2025-10-21 14:30:35 +0200

    new version test - 2025-10-21 14:30

--- update.sh
+++ update.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+# Check if description argument is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Description is required!"
+    echo "Usage: $0 \"<description>\""
+    exit 1
+fi
+
+DESCRIPTION="$1"
+
 cd ~/repo
 cp -r /var/www/html/ ./
 cp -r "/home/ubuntu/.wine/drive_c/Program Files (x86)/mForex Trader/MQL4/Experts/wd3.mq4" ./
 git add .
-git commit -m "Auto-update $(date +'%Y-%m-%d %H:%M')"
+git commit -m "$DESCRIPTION - $(date +'%Y-%m-%d %H:%M')"
 git push origin main
+
+echo "Update completed with description: $DESCRIPTION"
